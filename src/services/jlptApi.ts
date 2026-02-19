0561f1bf8e3e5b5430400394686b9c8041f59d66
Fahrendra Khoirul <fahrendra.khoirul@gmail.com>
2026-02-19 22:26:54 +0700

    change to N5 level

--- src/services/jlptApi.ts
+++ src/services/jlptApi.ts
@@ -2,16 +2,19 @@ import type { WordResponse } from "../types/word_response";
 
 const BASE_URL = "https://jlpt-vocab-api.vercel.app/api";
 
-export async function getWordsByLevel(level: number = 1): Promise<WordResponse[]> {
+export async function getWordsByLevel(
+  level: number = 5,
+): Promise<WordResponse[]> {
+  // random offset between 0 and 100
+  const offset = Math.floor(Math.random() * 100);
 
-    // random offset between 0 and 100
-    const offset = Math.floor(Math.random() * 100);
+  const response = await fetch(
+    `${BASE_URL}/words?level=${level}&limit=10&offset=${offset}`,
+  );
+  if (!response.ok) {
+    throw new Error("Failed to fetch words");
+  }
 
-    const response = await fetch(`${BASE_URL}/words?level=${level}&limit=10&offset=${offset}`);
-    if (!response.ok) {
-        throw new Error("Failed to fetch words");
-    }
-
-    const data = await response.json();
-    return data.words;
+  const data = await response.json();
+  return data.words;
 }
